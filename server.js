e2aeede8eb77b09766c0e3fd005daa641e482cd8
asudarsanan <asudarsanan@protonmail.com>
2020-12-31 14:20:02 +0530

    Fixed issue with server.js file

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/theApp'));
-app.get('/*',(req,res)=> res.sendFiles(path.join(__dirname)));
+app.get('/',(req,res)=> res.sendFile(path.join(__dirname)));
+
 const server = http.createServer(app);
 server.listen(port,()=> console.log('Running..'));
